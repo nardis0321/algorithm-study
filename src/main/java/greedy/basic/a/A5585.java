d448cd9bbf2c80f1bc70ee784424e37fbabd54f0
nardis0321 <nardis0321@gmail.com>
2025-04-16 21:38:47 +0900

    Solve 5585 거스름돈 (Greedy, Bronze 2)

---
+++ src/main/java/greedy/basic/a/A5585.java
@@ -0,0 +1,26 @@
+package greedy.basic.a;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class A5585 {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int amount = sc.nextInt();
+        int change = 1000 - amount;
+
+        int[] coins = {500, 100, 50, 10, 5, 1}; // 공간 복잡도 O(1)
+
+        int result = 0;
+        for (int coin : coins){     // 시간 복잡도 O(1)
+            result += change / coin;
+            change %= coin;
+        }
+
+        System.out.println(result);
+    }
+}
