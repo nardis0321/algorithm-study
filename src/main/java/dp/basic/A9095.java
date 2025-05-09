1a2a4199929ecef4ece84f18ebc5f123b94771ed
nardis0321 <nardis0321@gmail.com>
2025-05-09 20:53:47 +0900

    Refactor 9095 1, 2, 3 더하기 (DP, Silver 3)

--- src/main/java/dp/basic/A9095.java
+++ src/main/java/dp/basic/A9095.java
@@ -9,12 +9,7 @@ public class A9095 {
         // 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고,
         // 정수 n이 주어진다. n은 양수이며 11보다 작다.
         Scanner sc = new Scanner(System.in);
-        final int T = sc.nextInt();
-        int[] cases = new int[T+1];
-        for (int i = 1; i <= T; i++) {
-            cases[i] = sc.nextInt();
-        }
-
+        int T = sc.nextInt();
 
         // 큰 문제를 작은 문제로 나누기 : 1 2 3 으로 만들기 -> 마지막에 뭘 쓰는지
         // N -  1   2   3
@@ -24,40 +19,23 @@ public class A9095 {
         // 4    4   2   1
         // 5    7   4   2
         // n    dp[n-1] dp[n-2] dp[n-3]
-        int[][] dp = new int[12][4];    // n일 때 1만, 2까지, 3까지 사용
-        for (int i = 0; i < 12; i++) {
-            if(i == 1){
-                dp[i][1] = 1;
-                dp[i][2] = 0;
-                dp[i][3] = 0;
-            }
-            if(i == 2){
-                dp[i][1] = 1;
-                dp[i][2] = 1;
-                dp[i][3] = 0;
-            }
-            if(i == 3){
-                dp[i][1] = 2;
-                dp[i][2] = 1;
-                dp[i][3] = 1;
-            }
-            if(i >= 4){
-                for (int j = 1; j <= 3; j++) {
-                    dp[i][1] += dp[i-1][j];
-                    dp[i][2] += dp[i-2][j];
-                    dp[i][3] += dp[i-3][j];
-                }
-            }
+
+        // 기저 조건
+        int[] dp = new int[12];
+        dp[0] = 1;  // 편의상
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[4] = 4;
+
+        // 점화식 (바텀업)
+        for (int i = 4; i < 12; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
 
-        for (int i = 1; i < T+1; i++) {
-            int n = cases[i];
-            int result = 0;
-            for (int j = 1; j < 4; j++) {
-                result += dp[n][j];
-            }
-            System.out.println(result);
+        // 입력 > 출력
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            System.out.println(dp[n]);
         }
-
     }
 }
