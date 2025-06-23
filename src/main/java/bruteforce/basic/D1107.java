0eac388d19b67a5e095d3a17429f5bdf5cb2a9d5
nardis0321 <nardis0321@gmail.com>
2025-06-23 21:29:19 +0900

    Solve 1107 리모컨 (Brute Force, Gold 4)

--- src/main/java/bruteforce/basic/D1107.java
+++ src/main/java/bruteforce/basic/D1107.java
@@ -1,62 +1,40 @@
 package bruteforce.basic;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class D1107 {
     static int N;
     static int M;
 
-    static int find(int[] broken){
+    static int find(Set<Integer> brokenSet){
         if(N == 100){
             return 0;
         }
 
-        int cntGap;
-        if(N < 100){
-            cntGap = 100 - N;
-        } else {
-            cntGap = N - 100;
-        }
+        int result = Math.abs(N - 100); // +,- 버튼만 사용
 
-        if(M == 10){
-            return cntGap;
-        }
+        // 고장 안 난 숫자 선택해서
+        // + - 하기
+        // 다음 루프 최대 100만 번 반복
+        for (int i = 0; i <= 999999 ; i++) {    // N은 최대 500000이므로 999999로부터 이동하는 케이스까지 있을 수 있음
+            String str = String.valueOf(i);
+            boolean isValid = true;
+            for(char c : str.toCharArray()){    // 최대 6자리에 대한 O(6)
+                if(brokenSet.contains(c - '0')){
+                    isValid = false;
+                    break;
+                }
+            }
 
-        int n = N;
-        int cntMinus = 0;
-        int i = 0;
-        while(i < M){   // 최악 시간 복잡도 : O(N × M × D) = O(500,000 × 10 × 6) = 약 30,000,000 (3천만) 연산
-            String brokenStr = String.valueOf(broken[i]);
-            if (Integer.toString(n).contains(brokenStr)) {
-                n--;
-                cntMinus++;
-                i = 0;
-            } else {
-                i++;
+            if(isValid){
+                int cnt = str.length() + Math.abs(N - i);
+                result = Math.min(result, cnt);
             }
         }
-        cntMinus += Integer.toString(n).length();
 
-        int cntPlus = 0;
-        n = N;
-        i = 0;
-        while(i < M){
-            if(n == -1){
-                break;
-            }
-            String brokenStr = String.valueOf(broken[i]);
-            if (Integer.toString(n).contains(brokenStr)) {
-                n++;
-                cntPlus++;
-                i = 0;
-            } else {
-                i++;
-            }
-        }
-        cntPlus += Integer.toString(n).length();
-
-
-        return Math.min(Math.min(cntMinus, cntPlus), cntGap);
+        return result;
     }
 
 
@@ -76,13 +54,13 @@ public class D1107 {
         // 둘째 줄에는 고장난 버튼의 개수 M (0 ≤ M ≤ 10)이 주어진다.
         M = sc.nextInt();
         // 고장난 버튼이 있는 경우에는 셋째 줄에는 고장난 버튼이 주어지며, 같은 버튼이 여러 번 주어지는 경우는 없다.
-        int[] broken = new int[M];
+        Set<Integer> brokenSet = new HashSet<>(M);
         for (int i = 0; i < M; i++) {
-            broken[i] = sc.nextInt();
+            brokenSet.add(sc.nextInt());
         }
 
         // 첫째 줄에 채널 N으로 이동하기 위해 버튼을 최소 몇 번 눌러야 하는지를 출력한다.
-        int result = find(broken);
+        int result = find(brokenSet);
         System.out.println(result);
     }
 }
