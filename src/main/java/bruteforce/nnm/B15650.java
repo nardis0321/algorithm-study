a5a71f92d578baa447f8612797c1558dc49167f3
nardis0321 <nardis0321@gmail.com>
2026-02-24 22:26:39 +0900

    Refactor 15650 N과 M (2) (Bruteforce, Silver 3)

--- src/main/java/bruteforce/nnm/B15650.java
+++ src/main/java/bruteforce/nnm/B15650.java
@@ -11,22 +11,23 @@ public class B15650 {
     // 중복 x
     // 오름차순
 
+    static int n, m;
     static int[] permutation;
-    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
 
         permutation = new int[m];
-        visited = new boolean[n];
-        dfs(0, n, m);
+        dfs(0, 1);
         System.out.println(sb);
     }
 
-    static void dfs(int depth, int n, int m){
+    // Key : 이전보다 큰 것만 고르자
+    // start : 다음에 고를 수 있는 최소 숫자
+    static void dfs(int depth, int start){
         // 종료 조건
         if(depth == m){
             for (int i = 0; i < m-1; i++) {
@@ -38,16 +39,9 @@ public class B15650 {
         }
 
         // 선택, 재귀, 복구
-        for (int i = 0; i < n; i++) {
-            if(!visited[i]){
-                if(depth >= 1 && permutation[depth-1] > i+1) continue;
-                permutation[depth] = i+1;
-                visited[i] = true;
-
-                dfs(depth+1, n, m);
-
-                visited[i] = false;
-            }
+        for (int i = start; i <= n; i++) { // n과 m (1): 일단 선택 후 버림 > (2): 가능한 것만 선택
+            permutation[depth] = i;
+            dfs(depth+1, i+1);
         }
 
     }
