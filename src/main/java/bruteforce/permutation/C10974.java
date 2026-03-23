6badbfe32d59e2080c12ddc73f9eb415a41e679d
nardis0321 <nardis0321@gmail.com>
2026-03-23 19:03:45 +0900

    Refactor 10974 모든 순열 : Next Permutation Algorithm

--- src/main/java/bruteforce/permutation/C10974.java
+++ src/main/java/bruteforce/permutation/C10974.java
@@ -9,8 +9,8 @@ public class C10974 {
      * 1부터 N까지의 수로 이루어진 순열을 사전순으로 출력
      */
 
-    // 백트래킹
-    // 메모리 53,104KB	시간 1,652ms
+    // 백트래킹 : 메모리 53,104KB	시간 1,652ms
+    // Next Permutation : 백트래킹이랑 비교해서 코드길이 두 배, 메모리 절반, 시간 1/5
     static int n;
     static int[] permutation;
     static boolean[] visited;
@@ -19,9 +19,27 @@ public class C10974 {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         permutation = new int[n];
-        visited = new boolean[n];
+//        visited = new boolean[n];
+//
+//        dfs(0);
 
-        dfs(0);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            permutation[i] = i+1;
+        }
+
+        boolean flag = true;
+        while (flag){
+            for (int i = 0; i < n; i++) {
+                sb.append(permutation[i]).append(" ");
+            }
+            sb.append("\n");
+            flag = findNextPermutation();
+        }
+
+        System.out.println(sb);
     }
 
     static void dfs(int depth){
@@ -42,5 +60,56 @@ public class C10974 {
                 visited[i] = false;
             }
         }
+    }
+
+    static boolean findNextPermutation(){
+        int pivot = findPivot();
+        if(pivot == -1) return false;
+
+        int idx = n-1;
+        int min = n+1;
+        int minIdx = 0;
+        // 오른쪽에서 제일 작은거, 자신보다 큰 거
+        while(idx > pivot){
+            if(permutation[idx] < permutation[pivot]){
+                idx--;
+                continue;
+            }
+            if(permutation[idx] < min){
+                min = permutation[idx];
+                minIdx = idx;
+            }
+            idx--;
+        }
+        swap(pivot, minIdx);
+
+        // reverse 오른쪽
+        int left = pivot+1;
+        int right = n-1;
+        while(left < right){
+            swap(left, right);
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    static void swap(int a, int b){
+        int temp = permutation[a];
+        permutation[a] = permutation[b];
+        permutation[b] = temp;
+    }
+
+    static int findPivot(){
+        int pivot = -1;
+        int idx = n-1;
+        while(idx > 0){
+            if(permutation[idx-1] < permutation[idx]){
+                pivot = idx-1;
+                break;
+            }
+            idx--;
+        }
+        return pivot;
     }
 }
